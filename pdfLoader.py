5ab28f2194f97b7c5122731aed99fd25c59c20b7
sabelaV <79286097+sabelaV@users.noreply.github.com>
2024-11-13 14:52:13 +0100

    Update pdfLoader.py

--- pdfLoader.py
+++ pdfLoader.py
@@ -1,12 +1,11 @@
 import streamlit as st
-from langchain_groq import ChatGroq
+from langchain_groq import ChatGroq, GroqEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
-from chromadb import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
 
 # Interfaz de usuario en Streamlit para ingresar la clave de API de Groq
 st.title("Análisis de Documentos PDF con LangChain y Groq")
@@ -17,10 +16,12 @@ groq_api_key = st.text_input("Introduce tu Groq API Key", type="password")
 
 # Continuar solo si la clave de Groq está disponible
 if groq_api_key:
-    # Instancia del modelo ChatGroq
+    # Configurar el modelo de chat y embeddings de Groq
     chatModel = ChatGroq(
-        model="llama3-70b-8192"  # Ajusta el nombre del modelo según el que quieras usar
+        model="llama3-70b-8192",  # Ajusta el nombre del modelo según el que quieras usar
+        api_key=groq_api_key
     )
+    groq_embeddings = GroqEmbeddings(api_key=groq_api_key)
     
     # Interfaz de carga de archivos PDF
     uploaded_file = st.file_uploader("Sube un archivo PDF", type="pdf")
@@ -33,12 +34,8 @@ if groq_api_key:
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         splits = text_splitter.split_documents(docs)
     
-        # Crear el vectorstore y el recuperador
-        vectorstore = Chroma.from_documents(
-            documents=splits,
-            embedding=OpenAIEmbeddings(),
-            persist_directory=None  # Configuración en memoria
-        )
+        # Crear el vectorstore y el recuperador con Groq embeddings
+        vectorstore = Chroma.from_documents(documents=splits, embedding=groq_embeddings, persist_directory=None)
         retriever = vectorstore.as_retriever()
     
         # Definir el sistema y prompt de usuario
